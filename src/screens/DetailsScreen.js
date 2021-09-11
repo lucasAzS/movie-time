202771330d31cb02087ce0a3235f7d287250b9ec
Lucas de Azambuja dos Santos <lucasazsantos@gmail.com>
2021-09-10 23:17:38 -0300

    Access Web APIs with XMLHttpRequest

--- src/screens/DetailsScreen.js
+++ src/screens/DetailsScreen.js
@@ -1,37 +1,43 @@
 import React from 'react';
 import { Button, View, Text, StyleSheet } from 'react-native';
 
-const DetailsScreen = ( {navigation, route} ) => {
-    console.log(route);
-    const movie = route.params.movie;
-    
-    return (
-      <View style={styles.mainView}>
-        <Text style={{fontSize: 20}}>{movie.title} ({movie.release})</Text>
-        <Text style={{fontSize: 100}} >{movie.screenNumber}</Text>
-        <Button
-          title="Go to Image"
-          onPress={() => {navigation.navigate("BigImageView")}}/>
-        <Button
-          title="More Details"
-          onPress={() => {
-            movie.screenNumber = movie.screenNumber + 1;
-            console.log(movie);
-            navigation.push("Details_to_Details",
-                            {movie: movie} )}}/>
-        <Button
-          title="Go Back to Home"
-          onPress={() => {navigation.popToTop()}}/>
-      </View>
-    )
+const DetailsScreen = ({ navigation, route }) => {
+  console.log(route);
+  const movie = route.params.movie;
+  const [movieDetails, setMovieDetails] = React.useState(null);
+
+  React.useEffect(() => {
+    const xhr = new XMLHttpRequest();
+    xhr.open(
+      'GET',
+      'http://www.omdbapi.com/?apikey=957aa0ee&t=Star+Wars&y=1977'
+    );
+    xhr.send();
+    xhr.onload = () => {
+      if (xhr.status === 200) {
+        const response = JSON.parse(xhr.response);
+        setMovieDetails(response);
+      } else {
+        console.log(`http Request Failed ${xhr.status}`);
+      }
+    };
+  }, []);
+  return (
+    <View style={styles.mainView}>
+      <Text>{movieDetails === null ? '' : movieDetails.Title}</Text>
+      <Text>{movieDetails === null ? '' : movieDetails.Released}</Text>
+      <Text>{movieDetails === null ? '' : movieDetails.Plot}</Text>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
   mainView: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  }
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
+  },
 });
 
 export default DetailsScreen;
